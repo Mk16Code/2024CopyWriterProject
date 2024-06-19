79b95b2dd20eb052d107522cf5b97f4a920a9723
Moiz Kapadia <moizkapadia16@outlook.com>
2024-06-19 19:04:40 +0200

    This is the base setup

---
+++ 2024CopyWriter/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace _2024CopyWriter.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
